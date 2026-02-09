26a16cd64040409b91ce186dfb0038880dc7f21c
leon <angli9508@outlook.com>
2026-02-09 21:59:44 +0800

    Initial Commit

---
+++ LockIn/LockInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockInApp.swift
+//  LockIn
+//
+//  Created by leon on 2026/2/9.
+//
+
+import SwiftUI
+
+@main
+struct LockInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
